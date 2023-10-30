286a04060974827e774d3f9f390edab60c9beb3a
Verdiyanttt_ <verdiyant18@gmail.com>
2023-10-30 19:20:52 +0700

    "Keliling Kubus"

--- src/KelilingKubus.java
+++ src/KelilingKubus.java
@@ -1,5 +1,26 @@
-public class Main {
+import java.util.Scanner;
+
+public class KelilingKubus {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan panjang sisi kubus");
+        double sisi = input.nextDouble();
+
+        double keliling = hitungKelilingKubus(sisi);
+        System.out.println("Keliling kubus dengan sisi " + sisi + " adalah " + keliling);
+
+        input.close();
+        System.out.println();
     }
+
+    /**
+     *
+     * @param sisi salah satu dari rumus untuk penginputan nilai ruas
+     * @return untuk perhitungan keliling kubus tersebut
+     */
+    public static double hitungKelilingKubus(double sisi){
+
+        return 12 * sisi;
+    }
+
 }
